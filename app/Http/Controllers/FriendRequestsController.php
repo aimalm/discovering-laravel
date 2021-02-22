70776cdfae58e3b32dc90383b6986f67b863ea6a
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2021-02-22 08:13:14 +0100

    WIP for the search box + add friends feature

---
+++ app/Http/Controllers/FriendRequestsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\friend_requests;
+use Illuminate\Http\Request;
+
+class FriendRequestsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\friend_requests  $friend_requests
+     * @return \Illuminate\Http\Response
+     */
+    public function show(friend_requests $friend_requests)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\friend_requests  $friend_requests
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(friend_requests $friend_requests)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\friend_requests  $friend_requests
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, friend_requests $friend_requests)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\friend_requests  $friend_requests
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(friend_requests $friend_requests)
+    {
+        //
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <form class="d-flex">
+    <button class="btn-sm btn-outline-success" type="submit">Search</button>
 
+      <input class="form-control-sm me-2" type="search" placeholder="Search" aria-label="Search">
+    </form>
+  </div>
+</nav>
 @endsection
